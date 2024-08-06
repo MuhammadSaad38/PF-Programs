983ad1e4613157a7c5a41489d3ca656d791bc2b3
Muhammad Saad <149876788+MuhammadSaad38@users.noreply.github.com>
2024-08-06 11:32:31 +0500

    some updates

--- labs/lab#01/program-03-a.cpp
+++ labs/lab#01/program-03-a.cpp
@@ -1,8 +1,13 @@
 // 3. Display following sequences of stars in following ways:
-// a) Display them individually by writing four C++ codes and by writing just one cout statements
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "*********\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*********";  
+    // a) Display them individually by writing four C++ codes and by writing just one cout statements
+    
+    // b) Display them individually by writing four C++ codes and by writing multiple cout statements
+    cout << "*********\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*********" << endl; 
+    cout << "   ***\n *\t*\n*\t *\n*\t *\n*\t *\n*\t *\n*\t *\n *\t*\n   ***" << endl;
+	cout << "  *\n ***\n*****\n  *\n  *\n  *\n  *\n  *\n  *" << endl;
+	cout << "    *\n   * *\n  *   *\n *     *\n*\t*\n *     *\n  *   *\n   * *\n    *" << endl; 
     return 0;
 }
